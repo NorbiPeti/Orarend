58ea4934b8c0916bd917896b70b5c125d088df6d
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-01-30 14:22:25 +0100

    Added classes

---
+++ Orarend/Orarend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orarend
+{
+    public static class Orarend
+    {
+        public static IEnumerable<Óra> Órák(string csoportok)
+        {
+
+        }
+    }
+}
---
+++ Orarend/Óra.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orarend
+{
+    public class Óra
+    {
+        public string Azonosító { get; set; }
+        public string Név { get; set; }
+        public Tanár Tanár { get; set; }
+        public int Sorszám { get; set; }
+        public string Terem { get; set; }
+        public string Csoport { get; set; }
+    }
+}
