ec1188de66a2c30de1a859e72b5738f7ceb7c8d6
NorbiPeti <szatmari.norbert.peter@gmail.com>
2017-01-31 22:22:05 +0100

    Starting to figure things out
    Reminder: If the build hangs, check if it downloads something to C:\Users\Norbi\AppData\Local\Xamarin\zips

---
+++ OrarendAndroidApp/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.View;
+using Orarend;
+
+namespace OrarendAndroidApp
+{
+    [Activity(Label = "OrarendAndroidApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            ViewPager viewPager = FindViewById<ViewPager>(Resource.Id.viewpager);
+            API.Órák("");
+        }
+    }
+}
